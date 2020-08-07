5d156741df062f03d88dea6479dcd7450d71bfda
King Gao <king.gao@huawei.com>
2020-08-07 11:05:59 +0800

    add new file

--- learngit.py
+++ learngit.py
@@ -1,1 +1,12 @@
-try 
+try :
+    file = open("love","r+")
+except Exception as a:
+        print("there is no love file")
+        c = input("do you want creat a new file?")
+        if c == "y":
+            file = open("love","w")
+        else:
+            pass
+else:
+    file.write("I love xixi")
+file.close()
